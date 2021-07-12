3ead4799a7073312a6640bc03eb62263c9fb730e
Olga Marco <olga.marco@creublanca.es>
2021-07-12 12:57:32 +0200

    [MIG] sale_commission_delegated_partner: Migration to 13.0

--- sale_commission_delegated_partner/models/sale_commission_settlement.py
+++ sale_commission_delegated_partner/models/sale_commission_settlement.py
@@ -7,20 +7,14 @@ from odoo import _, models
 class SaleCommissionSettlement(models.Model):
     _inherit = "sale.commission.settlement"
 
-    def _prepare_invoice_header(self, settlement, journal, date=False):
-        vals = super()._prepare_invoice_header(settlement, journal, date=date)
-        if not settlement.agent.delegated_agent_id:
+    def _prepare_invoice(self, journal, product, date=False):
+        vals = super()._prepare_invoice(journal, product, date=date)
+        if not self.agent_id.delegated_agent_id:
             return vals
-        invoice = self.env["account.invoice"].new(vals)
-        invoice.partner_id = settlement.agent.delegated_agent_id
+        invoice = self.env["account.move"].new(vals)
+        invoice.partner_id = self.agent_id.delegated_agent_id
         invoice._onchange_partner_id()
-        invoice._onchange_journal_id()
+        invoice._onchange_journal()
+        for line in invoice.invoice_line_ids:
+            line.name += "\n" + _("Agent: %s") % self.agent_id.display_name
         return invoice._convert_to_write(invoice._cache)
-
-    def _prepare_invoice_line(self, settlement, invoice, product):
-        invoice_line_vals = super()._prepare_invoice_line(settlement, invoice, product)
-        if settlement.agent.delegated_agent_id:
-            invoice_line_vals["name"] += (
-                "\n" + _("Agent: %s") % settlement.agent.display_name
-            )
-        return invoice_line_vals
